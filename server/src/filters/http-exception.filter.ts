18f15cd37c37d672c350be7161869e63231c72ea
Vasil Dininski <dininski@gmail.com>
2018-12-21 17:59:30 +0200

    Handle deeplinking to React app
    Return react app for 404 routes

---
+++ server/src/filters/http-exception.filter.ts
@@ -0,0 +1,28 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import * as path from 'path';
+
+@Catch(HttpException, Error)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+    const status = exception.getStatus();
+
+    console.log(request.headers);
+    if (
+      status === 404 &&
+      request.headers.accept &&
+      request.headers.accept.indexOf('text/html') !== -1
+    ) {
+      return response.sendFile(path.resolve('./build/index.html'));
+    }
+
+    return response.status(exception.getStatus()).json(exception.getResponse());
+  }
+}
