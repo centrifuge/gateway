b9bbcc72620e28fad7dda9861f1ad11bfeac2500
razvan <razvan@centrifuge.io>
2019-06-07 15:12:42 +0200

    add ticks

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 GIT_SHORT_COMMIT=`git rev-parse --short HEAD`
-BRANCH_ABBR=git rev-parse --abbrev-ref HEAD
+BRANCH_ABBR=`git rev-parse --abbrev-ref HEAD`
 TRAVIS_BRANCH=${TRAVIS_BRANCH:-$BRANCH_ABBR}
 TIMESTAMP=`date -u +%Y%m%d%H%M%S`
 TAG="${TRAVIS_BRANCH}-${TIMESTAMP}-${GIT_SHORT_COMMIT}"
