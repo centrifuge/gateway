ab325174a76f282f123c4fad24b68d4e73a5eb24
Charly <charly@centrifuge.io>
2019-06-27 12:21:31 +0200

    module imports

--- server/src/transfers/transferdetails.module.ts
+++ server/src/transfers/transferdetails.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
 import { EthService } from "../eth/eth.service";
 import { TransferDetailsController } from "./transferdetails.controller";
+import { DatabaseModule } from "../database/database.module";
+import { AuthModule } from "../auth/auth.module";
+import { CentrifugeModule } from "../centrifuge-client/centrifuge.module";
 
 @Module({
   controllers: [TransferDetailsController],
+  imports: [DatabaseModule, AuthModule, CentrifugeModule],
   providers: [EthService],
 })
 export class TransferModule {}
