d266a58efb76652f15ce45ae3257051bf85fd0f6
Charly <charly@centrifuge.io>
2019-07-12 14:35:04 +0200

    wip

--- server/src/documents/documents.controller.spec.ts
+++ server/src/documents/documents.controller.spec.ts
@@ -6,6 +6,8 @@ import { DatabaseService } from "../database/database.service";
 import { DocumentsController } from "./documents.controller";
 import { centrifugeServiceProvider } from "../centrifuge-client/centrifuge.module";
 import { CoreapiCreateDocumentRequest } from "../../../clients/centrifuge-node";
+import {InvoicesController} from "../invoices/invoices.controller";
+import {Invoice} from "../../../src/common/models/invoice";
 
 describe('DocumentsController', () => {
   let documentsModule: TestingModule;
@@ -100,7 +102,67 @@ describe('DocumentsController', () => {
       expect(result.length).toEqual(2);
       expect(databaseSpies.spyGetAll).toHaveBeenCalledTimes(1);
     });
-
   });
 
-});
+  describe('update', function() {
+    it('should update the specified document', async function() {
+      const documentsController = documentsModule.get<DocumentsController>(
+          DocumentsController,
+      );
+
+      const updatedDocument: FlexDocument = {
+        ...documentToCreate,
+      };
+
+      const updateResult = await documentsController.updateById(
+          { id: insertedInvoice._id },
+          { user: { _id: 'user_id', account: '0x4441122' } },
+          { ...updatedInvoice },
+      );
+
+      expect(databaseSpies.spyFindOne).toHaveBeenCalledWith({
+        _id: insertedInvoice._id,
+        ownerId: 'user_id',
+      });
+      expect(invoiceSpies.spyUpdate).toHaveBeenCalledWith(
+          '0x39393939',
+          {
+            data: { ...updatedInvoice },
+            write_access: [updatedInvoice.sender, updatedInvoice.recipient],
+          },
+          '0x4441122',
+      );
+
+      expect(updateResult).toMatchObject({
+        data: {
+          ...updatedInvoice,
+        },
+        write_access: [updatedInvoice.sender, updatedInvoice.recipient],
+      });
+    });
+  });
+
+  describe('get by id', function() {
+    it('should return the invoice by id', async function() {
+      const invoiceController = invoicesModule.get<InvoicesController>(
+          InvoicesController,
+      );
+
+      const result = await invoiceController.getById(
+          { id: insertedInvoice._id },
+          { user: { _id: 'user_id' } },
+      );
+      expect(databaseSpies.spyFindOne).toHaveBeenCalledWith({
+        _id: insertedInvoice._id,
+        ownerId: 'user_id',
+      });
+
+      expect(result).toMatchObject({
+        data: invoice,
+        header: {
+          document_id: '0x39393939',
+        },
+      });
+    });
+  });
+})
