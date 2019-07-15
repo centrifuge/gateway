d59900b3174fe4bc0a8b0fcce0bd123bc6be292a
razvan <razvan@centrifuge.io>
2019-07-15 12:28:07 +0200

    interface crud for documents

--- src/common/models/schema.ts
+++ src/common/models/schema.ts
@@ -1,45 +1,29 @@
-import { isValidAddress } from "ethereumjs-util";
+import { isValidAddress } from 'ethereumjs-util';
 
 export class Schema {
- constructor(
-   readonly name: string,
-   readonly attributes: Attribute[],
-   public registries: Registry[],
-   readonly _id?: string,
- ){
-   Schema.validateRegistryAddress(this)
-   Schema.validateAttributes(this)
- }
-
-  public static validateRegistryAddress(schema: Schema) {
-   schema.registries.forEach(registry => {
-     let valid = isValidAddress(registry.address)
-     if (!valid) {
-       throw new Error(`${registry.address } is not a valid registry address`);
-     }
-   })
+  constructor(
+    readonly name: string,
+    readonly attributes: Attribute[],
+    public registries?: Registry[],
+    readonly _id?: string,
+  ) {
+    Schema.validateRegistryAddress(this);
   }
 
-  public static validateAttributes(schema: Schema) {
-   if (!Array.isArray(schema.attributes)) {
-     throw new Error(`Attribute set is not an array`);
-   }
-   if (!schema.attributes || !schema.registries || !schema.name) {
-     throw new Error(`Schema must define attributes, registries, and schema name`);
-   }
-   if (schema.attributes && schema.attributes.length > 0) {
-     const refID = schema.attributes.filter(attr => attr.name === 'reference_id');
-     if (refID.length === 0) {
-       throw new Error(`Attributes do not contain a reference ID`);
-     }
-   }
+  public static validateRegistryAddress(schema: Schema) {
+    schema.registries && schema.registries.forEach(registry => {
+      let valid = isValidAddress(registry.address);
+      if (!valid) {
+        throw new Error(`${registry.address} is not a valid registry address`);
+      }
+    });
   }
 }
 
 export interface Attribute {
-  name: string,
   label: string,
-  type: AttrTypes.STRING | AttrTypes.TIMESTAMP | AttrTypes.INTEGER | AttrTypes.BYTES | AttrTypes.DECIMAL
+  name: string,
+  type: AttrTypes.STRING | AttrTypes.TIMESTAMP | AttrTypes.INTEGER | AttrTypes.BYTES | AttrTypes.DECIMAL,
 }
 
 export interface Registry {
@@ -51,7 +35,7 @@ export interface Registry {
 export enum AttrTypes {
   INTEGER = 'integer',
   DECIMAL = 'decimal',
-  BYTES = 'bytes',
   STRING = 'string',
   TIMESTAMP = 'timestamp',
+  BYTES = 'bytes',
 }
---
+++ src/documents/CreateDocument.tsx
@@ -0,0 +1,144 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import DocumentForm from './DocumentForm';
+import { createInvoice, resetCreateInvoice } from '../store/actions/invoices';
+import { Invoice } from '../common/models/invoice';
+import { RouteComponentProps, withRouter } from 'react-router';
+import { getContacts, resetGetContacts } from '../store/actions/contacts';
+import { InvoiceResponse, LabelValuePair } from '../common/interfaces';
+import { Box, Button, Heading } from 'grommet';
+import { LinkPrevious } from 'grommet-icons';
+import { User } from '../common/models/user';
+import { Preloader } from '../components/Preloader';
+import { RequestState } from '../store/reducers/http-request-reducer';
+import { CoreapiDocumentResponse, InvInvoiceData } from '../../clients/centrifuge-node';
+import { SecondaryHeader } from '../components/SecondaryHeader';
+import { getUserSchemas, mapContactsToLabelKeyPair } from '../store/derived-data';
+import { documentRoutes } from './routes';
+import { Attribute, Registry,Schema } from '../common/models/schema';
+import { getSchemasList, resetGetSchemasList } from '../store/actions/schemas';
+
+type Props = {
+  getContacts: typeof getContacts;
+  resetGetContacts: typeof resetGetContacts;
+  getSchemasList: typeof getSchemasList;
+  resetGetSchemasList: typeof resetGetSchemasList;
+  creatingDocument: RequestState<InvInvoiceData>;
+  contacts?: LabelValuePair[];
+  schemas?: Schema[];
+  loggedInUser: User;
+} & RouteComponentProps;
+
+
+type State = {
+  defaultDocument: CoreapiDocumentResponse
+}
+
+export class CreateDocument extends React.Component<Props, State> {
+
+  constructor(props) {
+    super(props);
+    const { loggedInUser } = props;
+    this.state = {
+      defaultDocument: {
+
+      },
+    };
+  }
+
+  componentDidMount() {
+    if (!this.props.contacts) {
+      this.props.getContacts();
+      this.props.getSchemasList();
+    }
+  }
+
+  componentWillUnmount() {
+    this.props.resetGetSchemasList();
+    this.props.resetGetContacts();
+  }
+
+  createInvoice = (document: CoreapiDocumentResponse) => {
+    //this.props.createInvoice(invoice);
+    this.setState({
+      defaultDocument: document,
+    });
+  };
+
+  onCancel = () => {
+    this.props.history.push(documentRoutes.index);
+  };
+
+  render() {
+
+    const { creatingDocument, contacts,schemas } = this.props;
+    const { defaultDocument } = this.state;
+
+    if (!this.props.contacts || !schemas!.length) {
+      return <Preloader message="Loading"/>;
+    }
+
+    if (creatingDocument.loading) {
+      return <Preloader message="Saving document" withSound={true}/>;
+    }
+
+
+    return (
+      <DocumentForm
+        document={defaultDocument}
+        schemas={schemas}
+        onSubmit={this.createInvoice}
+        contacts={contacts}
+      >
+        <SecondaryHeader>
+          <Box direction="row" gap="small" align="center">
+            <Link to={documentRoutes.index} size="large">
+              <LinkPrevious/>
+            </Link>
+            <Heading level="3">
+              {'New Document'}
+            </Heading>
+          </Box>
+
+          <Box direction="row" gap="medium">
+            <Button
+              onClick={this.onCancel}
+              label="Discard"
+            />
+
+            <Button
+              type="submit"
+              primary
+              label="Save"
+            />
+          </Box>
+        </SecondaryHeader>
+      </DocumentForm>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    loggedInUser: state.user.auth.loggedInUser,
+    creatingDocument: state.documents.create,
+    contacts: mapContactsToLabelKeyPair(state),
+    schemas: getUserSchemas(state),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {
+    createInvoice,
+    resetCreateInvoice,
+    getContacts,
+    resetGetContacts,
+    getSchemasList,
+    resetGetSchemasList,
+  },
+)(withRouter(CreateDocument));
+
+
