2370a6d6218f9b03bd141811b2a277720562105c
razvan <razvan@centrifuge.io>
2019-10-09 15:26:34 +0200

    add placeholder and defaultValue to attributes

--- packages/lib/src/models/__tests__/schema.test.ts
+++ packages/lib/src/models/__tests__/schema.test.ts
@@ -69,6 +69,28 @@ describe('Schema validations', () => {
       }).toThrow(AttributesErrors.TYPE_NOT_SUPPORTED);
     });
 
+    it('should fail when placeholder is not a string', () => {
+      expect(() => {
+        Schema.validateAttributes([{
+          name: 'test',
+          label: 'test',
+          type: 'test',
+          placeholder: 3,
+        } as any]);
+      }).toThrow(AttributesErrors.PLACEHOLDER_FORMAT);
+    });
+
+    it('should fail when defaultValue is not a string', () => {
+      expect(() => {
+        Schema.validateAttributes([{
+          name: 'test',
+          label: 'test',
+          type: 'test',
+          defaultValue: 33,
+        } as any]);
+      }).toThrow(AttributesErrors.DEFAULT_VALUE_FORMAT);
+    });
+
     it('should fail when options is not an array', () => {
       expect(() => {
         Schema.validateAttributes([{
